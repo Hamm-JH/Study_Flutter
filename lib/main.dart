f346f8a0b1fe3cc9b9268356f4113665f1fe9c68
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-11 07:33:47 +0900

    output

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
 void main() {
+  var name = '코드팩토리';
+  print(name);
+
+  // 변수값 변경 가능
+  name = '골든래빗';
+  print(name);
+
+  // 변수명 중복 불가
+  //var name = '김고은'; //  Error: 'name' is already declared in this scope.
   print('hello world1');
 }

e64b44eb86a83324bff02d5f57cfc6e4b0f59390
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-11 22:51:28 +0900

    list update

--- lib/main.dart
+++ lib/main.dart
@@ -17,5 +17,44 @@ void main() {
   // print(isDouble);
   // print(isTrue);
 
+  // 리스트에 넣을 타입을 <> 사이에 명시할 수 있습니다.
+  List<String> blackPinkList = ['리사', '지수', '제니', '로제'];
+  
+  print(blackPinkList);
+  print(blackPinkList[0]);  // 첫 원소 지점
+  print(blackPinkList[3]);  // 마지막 원소 지점
+
+  print(blackPinkList.length); // 길이 반환
+
+  blackPinkList[3] = '코드팩토리'; // 3번 인덱스값 변경
+  print(blackPinkList);
+
+
+
+  // final newList = blackPinkList.where(
+  //     (name) => name == '리사' || name == '지수',   // '지수', // '리사' 또는 '지수'만 유지
+  // );
+  //
+  // print(newList);
+  // print(newList.toList());
+
+  // final newBlackPink = blackPinkList.map(
+  //       (name) => '블랙핑크 $name',
+  // );
+  // print(newBlackPink);
+  //
+  // // Iterable을 List로 다시 변환하고 싶을 때 .toList() 사용
+  // print(newBlackPink.toList());
+
+  // // 리스트를 순회하며 값들을 더함
+  // final allMembers = blackPinkList.reduce((value, element) => value + ', ' + element);
+  //
+  // print(allMembers);
+
+  // // reduce() 함수와 마찬가지로 각 요소를 순회하며 실행합니다.
+  // final allMembers =
+  //     blackPinkList.fold(0, (value, element) => value + element.length);
+  //
+  // print(allMembers);
 
 }

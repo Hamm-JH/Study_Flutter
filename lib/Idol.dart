f3bf4152161015bb328473cc355c40fb3245065d
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-12 19:06:16 +0900

    getter, setter

--- lib/Idol.dart
+++ lib/Idol.dart
@@ -1,7 +1,15 @@
-class IdolOther {
-  // '_'로 변수명을 시작하면
-  // 프라이빗 변수를 선언할 수 있습니다.
-  String _name;
+class Idol {
+  String _name = '블랙핑크';
 
-  IdolOther(this._name);
+  // get 키워드를 사용해서 게터임을 명시합니다.
+  // 게터는 메서드와 다르게 매개변수를 전혀 받지 않습니다.
+  String get name {
+    return this._name;
+  }
+
+  // set 키워드를 사용해서 세터를 선언합니다.
+  // 세터는 매개변수로 딱 하나의 변수를 받을 수 있습니다.
+  set name(String name) {
+    this._name = name;
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,7 @@
 import 'package:flutter_book_study/Idol.dart';
 
-class IdolIn {
-  // '_'로 변수명을 시작하면
-  // 프라이빗 변수를 선언할 수 있습니다.
-  String _name;
-
-  IdolIn(this._name);
-}
-
 void main() {
-  IdolIn blackPink = IdolIn('블랙핑크');
-
-  // 같은 파일에서는 _name변수에 접근할 수 있지만
-  // 다른 파일에서는 _name 변수에 접근할 수 없습니다.
-  print(blackPink._name);
-
-  IdolOther bts = IdolOther('BTS');
-
-  // print(bts._name);
+  Idol blackPink = Idol();
+  blackPink.name = '에이핑크';  // 세터
+  print(blackPink.name);    // 게터
 }

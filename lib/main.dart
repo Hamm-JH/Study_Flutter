76cea01a8f28cba847be0026337127ba411e581d
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-12 16:45:11 +0900

    new class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 // import 'package:flutter/material.dart';
 
+import 'package:flutter_book_study/Idol.dart';
+
 void main() {
-  double? number; // 자동으로 null값 지정
-  print(number);
 
-  number ??= 3; // ??를 사용하면 기존 값이 null일 때만 저장됩니다.
-  print(number);
-
-  number ??= 4; // null이 아니므로 3이 유지됩니다.
-  print(number);
+  Idol blackPink = new Idol();
+  
+  // 메서드를 실행
+  blackPink.sayName();
 }

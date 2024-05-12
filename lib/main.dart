8b06df8602afd7eed9a39b8e2855a5c714387240
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-12 23:08:58 +0900

    generic

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flutter_book_study/GirlGroup.dart';
 
-void main() {
-  GirlGroup blackPink = GirlGroup('블랙핑크', 4);
+// 인스턴스화할 때 입력받을 타입을 T로 지정합니다.
+class Cache<T> {
+  // data의 타입을 추후 입력될 T 타입으로 지정합니다.
+  final T data;
 
-  blackPink.sayName();
-  blackPink.sayMembersCount();
+  Cache({
+    required this.data,
+  });
+}
+
+void main() {
+  // T의 타입을 List<int>로 입력합니다.
+  final cache = Cache<List<int>> (
+    data: [1, 2, 3],
+  );
+
+  // 제네릭에 입력된 값을 통해 data 변수의 타입이 자동으로 유추합니다.
+  // reduce() 함수가 기억나지 않는다면 1.4.1절 'List 타입'을 복습
+  print(cache.data.reduce((value, element) => value + element));
 }

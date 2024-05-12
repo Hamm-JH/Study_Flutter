a1145a1f5bc2871dcfb07d88329de14b880a81c0
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-12 19:29:25 +0900

    inheritence

--- lib/Idol.dart
+++ lib/Idol.dart
@@ -1,15 +1,14 @@
 class Idol {
-  String _name = '블랙핑크';
+  final String name;
+  final int membersCount;
 
-  // get 키워드를 사용해서 게터임을 명시합니다.
-  // 게터는 메서드와 다르게 매개변수를 전혀 받지 않습니다.
-  String get name {
-    return this._name;
+  Idol(this.name, this.membersCount);
+
+  void sayName() {
+    print('저는 ${this.name}입니다.');
   }
 
-  // set 키워드를 사용해서 세터를 선언합니다.
-  // 세터는 매개변수로 딱 하나의 변수를 받을 수 있습니다.
-  set name(String name) {
-    this._name = name;
+  void sayMembersCount() {
+    print('${this.name} 멤버는 ${this.membersCount}명입니다');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:flutter_book_study/BoyGroup.dart';
 import 'package:flutter_book_study/Idol.dart';
 
 void main() {
-  Idol blackPink = Idol();
-  blackPink.name = '에이핑크';  // 세터
-  print(blackPink.name);    // 게터
+  BoyGroup bts = BoyGroup('BTS', 7); // 생성자로 객체 생성
+  
+  bts.sayName();    // 부모한테 물려받은 메서드
+  bts.sayMembersCount();  // 부모한테 물려받은 메서드
+  bts.sayMale();    // 자식이 새로 추가한 메서드
 }

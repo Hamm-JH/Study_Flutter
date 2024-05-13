da8718df5c15c34e0f3f5321b5c9f3e1b070afa6
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-13 16:51:56 +0900

    first flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
+import 'package:flutter/material.dart';
 
 void main() {
-  addNumbers(1, 1);
-}
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
 
-void addNumbers(int number1, int number2) {
-  print('$number1 + $number2 계산 시작!');
-
-  // 1 Future.delayed()를 사용하면 일정 시간 후에 콜백 함수를 실행할 수 있음
-  Future.delayed(Duration(seconds: 3), () {
-    print('$number1 + $number2 = ${number1 + number2}');
-  });
-
-  print('$number1 + $number2 코드 실행 끝');
+            // 1 하나의 위젯만 가운데 정렬 가능
+            'Hello Code Factory'
+          ),
+        ),
+      )
+    )
+  );
 }

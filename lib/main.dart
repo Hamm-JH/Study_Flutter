7a18d49481f58aea8a7306fb530125da20be30ff
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-13 09:23:26 +0900

    0513 update

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,4 @@
-import 'package:flutter_book_study/GirlGroup.dart';
-
-class Counter {
-  // 1 static 키워드를 사용해서 static 변수 선언
-  static int i = 0;
-
-  // 2 static 키워드를 사용해서 static 변수 선언
-  Counter() {
-    i++;
-    print(i++);
-  }
-}
 
 void main() {
-  Counter count1 = Counter();
-  Counter count2 = Counter();
-  Counter count3 = Counter();
+
 }

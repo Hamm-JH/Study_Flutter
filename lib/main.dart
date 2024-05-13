54ce726909dff8ac66de4dff7805efdf99ec0e7f
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-13 17:47:01 +0900

    gesture widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(FloatingActionButtonExample());
+  // runApp(MyApp());
+}
+
+class FloatingActionButtonExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          // 클릭했을 때 실행할 함수
+          onPressed: () {},
+          child: Text('클릭'),
+        ),
+        body: Container(),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {

3c68ebf66829d6b98fd954ec8d3d00400e2f497f
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-13 11:02:51 +0900

    async await return

--- lib/async_await.dart
+++ lib/async_await.dart
@@ -1,17 +1,18 @@
 
 void main() async {
-  await addNumbers(1, 1);
-  await addNumbers(2, 2);
+  final result = await addNumbers(1, 1);
+  print('결과값 $result'); // 일반 함수와 동일하게 반환값을 받을 수 있음
+  final result2 = await addNumbers(2, 2);
+  print('결과값 $result2');
 }
 
-// async 키워드는 함수 매개변수 정의와 바디 사이에 입력합니다.
-Future<void> addNumbers(int number1, int number2) async {
+Future<int> addNumbers(int number1, int number2) async {
   print('$number1 + $number2 계산 시작!');
 
-  // await는 대기하고 싶은 비동기 함수 앞에 입력합니다.
   await Future.delayed(Duration(seconds: 3), () {
     print('$number1 + $number2 = ${number1 + number2}');
   });
-
   print('$number1 + $number2 코드 실행 끝');
+
+  return number1 + number2;
 }

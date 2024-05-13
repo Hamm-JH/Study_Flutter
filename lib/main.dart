b9ce6536f9d1d7c27d95e53607a30c0442f95ec1
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-13 16:56:33 +0900

    children widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text(
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
 
-            // 1 하나의 위젯만 가운데 정렬 가능
-            'Hello Code Factory'
+            // 1 여러 위젯을 Column 위젯에 입력 가능
+            children: [
+              Text('Code'),
+              Text('Factory'),
+              Text('Howard')
+            ],
           ),
         ),
-      )
-    )
+      ),
+    ),
   );
 }

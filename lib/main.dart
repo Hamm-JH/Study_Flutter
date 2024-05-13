6ce24f2d21b650ed3a64978c4b8bba44a8a5777f
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-13 17:36:28 +0900

    widget buttons 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SizedBox(
-          width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+  runApp(MyApp());
+}
 
-            // 1 여러 위젯을 Column 위젯에 입력 가능
-            children: [
-              Text('Code'),
-              Text('Factory'),
-              Text('Howard')
-            ],
-          ),
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: GestureDetector(
+            // 1 한 번 탭했을 때 실행할 함수
+            onTap: () {
+              // 출력 결과는 안드로이드 스튜디오의 [Run] 탭에서 확인 가능합니다.
+              print('on tap');
+            },
+            // 2 두 번 탭했을 때 실행할 함수
+            onDoubleTap: () {
+              print('on double tap');
+            },
+            // 3 길게 눌렀을 때 실행할 함수
+            onLongPress: () {
+              print('on long press');
+            },
+            // 제스처를 적용할 위젯
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+              ),
+              width: 100.0,
+              height: 100.0,
+            )
+          )
         ),
-      ),
-    ),
-  );
+      )
+    );
+
+  }
 }

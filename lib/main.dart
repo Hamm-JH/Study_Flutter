9591413178e217850535067bf5c3a78a457845dd
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-05-13 22:09:49 +0900

    row

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() {
   // runApp(FloatingActionButtonExample());
-  runApp(MyApp());
+  // runApp(MyApp());
+  runApp(RowWidgetExample());
+}
+
+class RowWidgetExample extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+
+          // 반대축에서 이동할 공간을 제공하기 위해 높이를 최대한으로 설정
+          height: double.infinity,
+          child: Row(
+
+            // 주축 정렬 지정
+            mainAxisAlignment: MainAxisAlignment.start,
+
+            // 반대축 정렬 지정
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            // 넣고 싶은 위젯 입력
+            children: [
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.red,
+              ),
+
+              // SizedBox는 일반적으로 공백을 생성할 때 사용
+              const SizedBox(width: 12.0),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.green,
+              ),
+
+              const SizedBox(width: 12.0),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.blue,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }  
 }
 
 class FloatingActionButtonExample extends StatelessWidget {
@@ -27,17 +76,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: SizedBox(
-            // 높이 지정
-            height: 200.0,
-            // 너비 지정
-            width: 200.0,
-
-            // SizedBox는 색상이 없으므로 크기를 확인하는
-            // 용도로 Container 추가
+          child: SafeArea(
+            // 원하는 부위만 따로 적용할 수도 있습니다.
+            // true는 적용 false는 미적용
+            top: true,
+            bottom: true,
+            left: true,
+            right: true,
             child: Container(
               color: Colors.red,
-            ),
+              height: 300.0,
+              width: 300.0,
+            )
           )
         ),
       )
